a5f7664ec7aad7063f28d6626ba51be0eb62f8ea
Steve Wins <Steve@Winss-MacBook-Pro.local>
2016-06-15 15:23:32 +0800

    数据接口说明

--- application/Home/Controller/IndexController.class.php
+++ application/Home/Controller/IndexController.class.php
@@ -3,13 +3,17 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-    	/*
-    	查询表的应用类型
-	    */
+
+    	/*查询表的应用类型*/
    		$arr = M('apply')->select();
    		$this->assign('apply',$arr);
-
    		
+
+
+
+
+
+
    		$this->display();	
     }
 }
