a72e54a2041a1f5463042a95cffe96897796b555
lff19920123 <18734832258@163.com>
2016-06-15 12:23:11 +0800

    6.15

---
+++ index.php
@@ -0,0 +1,11 @@
+﻿<?php
+	header('content-type:text/html;charset=utf-8');
+		//define('APP_NAME','getsql');//项目名称
+		define('APP_DEBUG',true);//开启调试模式
+		define('APP_PATH','./application/');//应用程序目录
+		define('COMMON_PATH','./Common/');
+		
+		// 绑定入口文件到Admin模块访问
+		//define('BIND_MODULE','Admin');
+		require './ThinkPHP/ThinkPHP.php';
+?>
