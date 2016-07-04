07746d7bbc604ab653412b7205d6555a630465df
libiao <libiao>
2016-07-04 19:25:54 +0800

    s

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 ﻿<?php
-	header('content-type:text/html;charset=utf-8');
-		//define('APP_NAME','getsql');//项目名称
-		define('APP_DEBUG',true);//开启调试模式
-		define('APP_PATH','./application/');//应用程序目录
-		define('COMMON_PATH','./Common/');
-		
-		// 绑定入口文件到Admin模块访问
-		//define('BIND_MODULE','Admin');
-		require '../ThinkPHP/ThinkPHP.php';
+	//开启调试模式
+	// define('APP_DEBUG',true);
+
+	// 应用程序目录
+	define('APP_PATH','./application/');
+
+	// 运行目录
+	define('COMMON_PATH','./Common/');
+	
+	// 绑定入口文件到Admin模块访问
+	//define('BIND_MODULE','Admin');
+	
+	require './ThinkPHP/ThinkPHP.php';
 ?>
